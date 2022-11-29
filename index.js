d346804aed7e6ea0904c7e3f1a98e50c4bf8d011
mamun-prodhan <mamunpr9@gmail.com>
2022-11-30 02:29:54 +0600

    get data based on category api added

--- index.js
+++ index.js
@@ -11,30 +11,53 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
 // mongodatabase connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nhx4fnh.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
+async function run() {
+    try {
+        const categoryCollection = client.db('carReseller').collection('categories');
+        const carsCollection = client.db('carReseller').collection('cars');
 
+        // get all categories data
+        app.get('/categories', async (req, res) => {
+            const query = {}
+            const cursor = categoryCollection.find(query);
+            const categories = await cursor.toArray();
+            res.send(categories);
+        })
 
+        //get data based on categoryName
+        app.get('/category', async (req, res) => {
+            let query = {};
+            if(req.query.categoryName){
+                query = {
+                    categoryName: req.query.categoryName
+                }
+            }
+            const cursor = carsCollection.find(query);
+            const cars = await cursor.toArray();
+            res.send(cars);
+        })
 
+    }
+    finally {
 
+    }
+}
+
+run().catch(err => console.error(err));
 
 
 
 
 // start 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.send('assignment 12 server is running')
 })
 
-
-
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`assignment 12 server running on ${port}`);
 })
